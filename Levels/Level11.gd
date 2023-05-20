671be17ea497201b256b910e97a3ce66317ce61c
G0aty695 <121062577+TheG0at685@users.noreply.github.com>
2023-05-20 18:41:05 -0400

    Added desert theme

--- Levels/Level11.gd
+++ Levels/Level11.gd
@@ -9,7 +9,7 @@ extends KinematicBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().current_scene.get_node("ParallaxBackground2").mode = "desert"
-	get_tree().current_scene.current_song = null
+	get_tree().current_scene.current_song = "res://Assets/sounds/The great desert theme.wav"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
